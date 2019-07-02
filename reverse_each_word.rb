b35314700bde85ff3dc0911d672b7afd7698b19f
Donovan Jones <donovanljones94@gmail.com>
2019-07-02 15:00:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
+  array = string.split(" ")
+  new_array = []
+  array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
